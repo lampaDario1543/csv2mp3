2bed024c13a9cb7ff4d29947ceaad2e7a313053e
tommy <tommaso.malinverno@icloud.com>
2023-10-06 21:58:14 +0200

    gestione degli errori, creazione exe di prova

--- main-v2.py
+++ main-v2.py
@@ -10,9 +10,15 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from datetime import datetime
 import time as t
 import sys #per prendere il path del file
+import msvcrt #per chiudere il programma
+from colorama import Fore, Style, init # per colorare gli errori
+import sys
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="5ac01819a18e445c824ea3120d950b13",
                                                            client_secret="15d8e6f00d0f4f858b4273d7456dd07c"))
+os.system("title csv2mp3")
+
+init()#coloram
 
 #caratteri proibiti e replace per il nome del file e della cartella
 
@@ -31,20 +37,29 @@ rules = [
 
 def getCsvPath(): #ottengo il percorso del file csv tramite argomenti
     if len(sys.argv) < 2:
-        print("Nome file automatico: titles.csv\n\n")
-        return "titles.csv"
+        print("Inserisci il percorso del file csv:")
+        path=input("> ")
+        if(not path.lower().endswith('.csv')):
+            print(f"{Fore.RED}Errore: Il file deve essere un csv.{Style.RESET_ALL}\n\n")
+            return None
+        elif(not os.path.exists(path)):
+            print(f"{Fore.RED}Errore: Il file non esiste.{Style.RESET_ALL}\n\n")
+            return None
+        return path
     path = ' '.join(sys.argv[1:])
     if(not path.lower().endswith('.csv')):
-         print("Errore: Il file deve essere un csv.\n\n")
-         return "titles.csv"
+        print(f"{Fore.RED}Errore: Il file deve essere un csv.{Style.RESET_ALL}\n\n")
+        return None
     if(not os.path.exists(path)):
-        print("Errore: Il file non esiste.\n\n")
-        return "titles.csv"
+        print(f"{Fore.RED}Errore: Il file non esiste.{Style.RESET_ALL}\n\n")
+        return None
     return path
 
 def run(path):
     songs=getCSV(path)
     DIM=len(songs)
+    if not os.path.exists('csv2mp3/'):
+        os.mkdir('csv2mp3/')
     for i in range(DIM):
         print("Download "+str(i+1)+" di "+str(DIM)+" - "+ str(((i+1)/DIM)*100)[0:4]+"% canzoni scaricate, mancanti: "+str(DIM-(i+1)))
         video_url = getUrl(songs[i]['title']+' '+songs[i]['artist'],songs[i])
@@ -57,15 +72,15 @@ def run(path):
 
         track=getInfo(songs[i]['title']+' '+songs[i]['artist'])
         if not os.path.exists(songs[i]['album']): #se la cartella dell'album non esiste
-            os.mkdir(songs[i]['album']) #creo la cartella col nome dell'album
-            img=open(songs[i]['album']+"/thumb.jpg","wb") #creo l'immagine dell'album
+            os.mkdir("csv2mp3/"+songs[i]['album']) #creo la cartella col nome dell'album
+            img=open("csv2mp3/"+songs[i]['album']+"/thumb.jpg","wb") #creo l'immagine dell'album
             response=requests.get(track['album']['images'][0]['url']) #richiedo l'url dell'immagine dell'album
             img.write(response.content) #scrivo l'immagine
             img.close() #chiudo l'immagine
         
         #scarico la canzone
 
-        filename = f"{songs[i]['album']}/{songs[i]['title']}.temp" #creo il file temporaneo
+        filename = f"csv2mp3/{songs[i]['album']}/{songs[i]['title']}.temp" #creo il file temporaneo
         video = YouTube(video_url) #ottengo il video
         audio_streams = video.streams.filter(only_audio=True)
         audio_streams = audio_streams.order_by('abr')
@@ -77,20 +92,20 @@ def run(path):
         #converto il file in mp3
 
         print("Conversione di "+songs[i]['title']+" in mp3")
-        audio=AudioSegment.from_file(songs[i]['album']+"/"+songs[i]['title']+".temp")
-        audio.export(songs[i]['album']+"/"+songs[i]['title']+".mp3", format="mp3")
-        os.remove(songs[i]['album']+"/"+songs[i]['title']+".temp")
+        audio=AudioSegment.from_file("csv2mp3/"+songs[i]['album']+"/"+songs[i]['title']+".temp")
+        audio.export("csv2mp3/"+songs[i]['album']+"/"+songs[i]['title']+".mp3", format="mp3")
+        os.remove("csv2mp3/"+songs[i]['album']+"/"+songs[i]['title']+".temp")
 
         #aggiungo le informazioni alla canzone
 
         print("\nAggiunta dati a: "+str(i+1))
-        audio=eyed3.load(songs[i]['album']+"/"+songs[i]['title']+".mp3")
+        audio=eyed3.load("csv2mp3/"+songs[i]['album']+"/"+songs[i]['title']+".mp3")
         audio.tag.title=track['name']
         audio.tag.album=track['album']['name']
         audio.tag.artist=songs[i]['artist']
         audio.tag.track_num=track['track_number'], track['album']['total_tracks']
         audio.tag.recording_date=int(track['album']['release_date'][0:4])
-        with open(songs[i]['album']+"/thumb.jpg", "rb") as cover_art:
+        with open("csv2mp3/"+songs[i]['album']+"/thumb.jpg", "rb") as cover_art:
             audio.tag.images.set(3, cover_art.read(), "image/jpeg")
         audio.tag.save()
         os.system('cls')
@@ -101,9 +116,14 @@ def run(path):
         last.write(track['name'])
         last.write("\n"+str(datetime.today()));
         last.close()
+    print("Download completato con successo!\n\n")
+    print("Premi un tasto per uscire...")
+    msvcrt.getch()
+    sys.exit()
 
 def getUrl(title, song): #ottengo l'url del video
     ytmusic = YTMusic()
+    ytmusic=YTMusic()
     url="https://music.youtube.com/watch?v="
     temp=str(ytmusic.search(title,"songs"))
     url+=temp[temp.find("videoId")+11:temp.find("videoId")+22]
@@ -111,23 +131,44 @@ def getUrl(title, song): #ottengo l'url del video
     return url
 
 
-
 def getCSV(file:str) -> list: #ritorno la lista di canzoni presa dal file csv
-	songs = []
-	with open(file,"r",encoding="utf-8") as csv_file:
-		csv_reader = csv.DictReader(csv_file)
-		for x in csv_reader:
-			songs.append(x)
-	return songs
+    songs = []
+    with open(file, "r", encoding="utf-8") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            songs.append(row)
+        if(len(songs)==0):
+            print(f"{Fore.RED}Errore: Il file '{file}' è vuoto.{Style.RESET_ALL}\n\n")
+            print("Premi un tasto per uscire...")
+            msvcrt.getch()
+            sys.exit()
+        required_fields = ['title', 'artist', 'album']
+        if(len(songs[0].keys())!=len(required_fields)):
+            print(f"{Fore.RED}Errore: I campi del file devono essere: {len(required_fields)} {Style.RESET_ALL}\n\n")
+            print("Premi un tasto per uscire...")
+            msvcrt.getch()
+            sys.exit()
+        for r in required_fields:
+            try:
+                songs[0][r]
+            except Exception as e:
+                print(f"{Fore.RED}Errore: I campi del file devono essere: {required_fields} {Style.RESET_ALL}\n\n")
+                print("Premi un tasto per uscire...")
+                msvcrt.getch()
+                sys.exit()
+    return songs
 
 def getInfo(title): #ritorno le informazioni della canzone
     results = sp.search(q=title, limit=1)
-
     for idx, track in enumerate(results['tracks']['items']):
         print(str(track['name']))
     return track
 
 if __name__=='__main__':
     path=getCsvPath() #ottengo il percorso del file csv
-    t.sleep(3)
-    run(path)
+    if(path==None):
+        print("Premi un tasto per uscire...")
+        msvcrt.getch()
+        sys.exit()
+    else:
+        run(path)
